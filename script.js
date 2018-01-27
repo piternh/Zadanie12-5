f3f5e8531e6eef7b0b7fbdc609a96f9b07690471
Piotr Cybulski <piotrekkc2@gmail.com>
2018-01-27 18:44:59 +0100

    Fix code in all files

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
-
 function getQuote() {
-    $.getJSON(quoteUrl, createTweet);
+    $.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({ cache: false });
 }
-
 
 function createTweet(input) {
     var data = input[0];
@@ -16,20 +16,21 @@ function createTweet(input) {
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
-    
-    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-    
+  
+  var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+  
+
+  
     if (tweetText.length > 140) {
-    getQuote();
-	} else {
-    var tweet = tweetLink + encodeURIComponent(tweetText);
-    $('.quote').text(quoteText);
-    $('.author').text("Author: " + quoteAuthor);
-    $('.tweet').attr('href', tweet);
-	}
-	
-	
+      getQuote();
+    } else {
+      var tweet = tweetLink + encodeURIComponent(tweetText);
+      $('.quote').text(quoteText);
+      $('.author').text("Author: " + quoteAuthor);
+      $('.tweet').attr('href', tweet);
+    }
 }
+
 
 
 $(document).ready(function() {
@@ -38,3 +39,4 @@ $(document).ready(function() {
         getQuote();
     })
 });
+
